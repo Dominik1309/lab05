7ba403837ea02aab757de3f5237984839b9728f1
Dominik1309 <dominik.duric@gmx.at>
2020-04-26 12:00:11 +0200

    Create welcome.sh

---
+++ welcome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# first line is called shebang, used to tell which 
+# author: Dominik Duric
+# change permissions to execute `chmod 755 `
+
+echo "Hello again `whoami`! Today is the `date`. The bash shell version is ${BASH_VERSION}"
